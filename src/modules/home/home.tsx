cc88cffc442dc7352857b8e8d2a7bb23b7e0e01a
delower-hasan <dhhasansaha11@gmail.com>
2025-06-09 22:29:47 +0600

    multicurrency page complete

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -13,6 +13,27 @@ import FAQSection from './FAQSection';
 import PromotionSection from './PromotionSection';
 
 const Home = () => {
+  const steps = [
+    {
+      id: 0,
+      title: 'Create your SmashPay account',
+      content:
+        "Start your registration online or via the SmashPay app. We'll ask for some personal information including your contact details, nationality and driver's license number (optional).",
+      icon: '/chevron-up0.svg'
+    },
+    {
+      id: 1,
+      title: 'Enter your transfer details',
+      content: 'Provide the details of your transfer including amount, currency, and recipient information.',
+      icon: '/chevron-down3.svg'
+    },
+    {
+      id: 2,
+      title: 'Send funds for your transfer',
+      content: 'Complete your transfer by sending the funds through your preferred payment method.',
+      icon: '/chevron-down4.svg'
+    }
+  ];
   return (
     <>
       <Hero />
@@ -23,7 +44,7 @@ const Home = () => {
       <SmashPayCard />
       <SmashPayApp />
       <CustomerStories />
-      <HowToGetStarted />
+      <HowToGetStarted steps={steps} title="It's simple to start using SmashPay" />
       <PromotionSection />
       <FAQSection />
       <CTASections />
