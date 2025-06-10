3b7b5faead7a51d222d71292e308dc4bb455dcf7
delower-hasan <dhhasansaha11@gmail.com>
2025-06-10 10:05:40 +0600

    about office section respoinsive

--- src/modules/about-us/components/OfficesSection.tsx
+++ src/modules/about-us/components/OfficesSection.tsx
@@ -1,36 +1,44 @@
 'use client';
 
+import useIsMobile from '@/hook/isMobile';
 import { motion } from 'framer-motion';
+import { useMemo } from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 
-const offices = [
-  {
-    name: 'Yogyakarta',
-    address: 'Jend. Sudirman Street, Jetis, Yogyakarta 55233',
-    image: '/about-us/yogyakartaJend.png',
-    styles: 'left-[120px] top-14 h-[323px]'
-  },
-  {
-    name: 'Berlin',
-    address: 'Level 8, Rosa-Luxemburg-Straße 71, Berlin 80094',
-    image: '/about-us/berlin.png',
-    styles: 'left-[120px] top-[419px] h-[260px]'
-  },
-  {
-    name: 'San Francisco',
-    address: '123 Street, 7th Floor,\nSan Francisco, CA 94105',
-    image: '/about-us/san-francisco.png',
-    styles: 'top-0 right-0 h-[322px]'
-  },
-  {
-    name: 'London',
-    address: 'The Warehouse 123-456\nOld St, London, EC1V 9NR UK',
-    image: '/about-us/london.png',
-    styles: 'top-[340px] right-0 h-[348px]'
-  }
-];
-
 export default function OfficesSection() {
+  const isMobile = useIsMobile();
+
+  const offices = useMemo(() => {
+    const data = [
+      {
+        name: 'Yogyakarta',
+        address: 'Jend. Sudirman Street, Jetis, Yogyakarta 55233',
+        image: '/about-us/yogyakartaJend.png',
+        styles: 'left-[120px] top-14 h-[323px]'
+      },
+      {
+        name: 'Berlin',
+        address: 'Level 8, Rosa-Luxemburg-Straße 71, Berlin 80094',
+        image: '/about-us/berlin.png',
+        styles: 'left-[120px] top-[419px] h-[260px]'
+      },
+      {
+        name: 'San Francisco',
+        address: '123 Street, 7th Floor,\nSan Francisco, CA 94105',
+        image: '/about-us/san-francisco.png',
+        styles: 'top-0 right-0 h-[322px]'
+      },
+      {
+        name: 'London',
+        address: 'The Warehouse 123-456\nOld St, London, EC1V 9NR UK',
+        image: '/about-us/london.png',
+        styles: 'top-[340px] right-0 h-[348px]'
+      }
+    ];
+
+    return isMobile ? data.slice(0, 2) : data;
+  }, [isMobile]);
+
   return (
     <section className='flex w-full flex-col items-center overflow-hidden bg-[#0f0f0f] pt-32 pb-32'>
       <div className='flex w-full max-w-[1280px] flex-col gap-16 px-4 md:px-8 lg:flex-row'>
