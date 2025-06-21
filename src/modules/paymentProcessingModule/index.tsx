511fe338b2875414d1837446ff14e18246f23a6e
delower-hasan <dhhasansaha11@gmail.com>
2025-06-22 00:38:52 +0600

    revert

--- src/modules/paymentProcessingModule/index.tsx
+++ src/modules/paymentProcessingModule/index.tsx
@@ -3,6 +3,9 @@ import CountryCoverageHome from '../home/components/CountryCoverageHome';
 import CTASections from '../home/components/CTASections';
 import FAQSection from '../home/FAQSection';
 import PromotionSection from '../home/PromotionSection';
+import ConfettieSubscription from './components/ConfettieSubscription';
+import ConfettiGenerateLink from './components/ConfettiGenerateLink';
+import ConfettiInstallment from './components/ConfettiInstallment';
 import KeyBenifits from './components/KeyBenifits';
 import PaymentOptions from './components/PaymentOptions';
 import ProcessingHero from './components/ProcessingHero';
@@ -16,12 +19,12 @@ const PaymentProcessingModule = () => {
       <SectionWhoAreWe />
       <KeyBenifits />
       <PromotionSection />
-      {/* <div className='bg-gradient m-auto flex w-full flex-col items-center justify-center gap-10 py-20'>
+      <div className='bg-gradient m-auto flex w-full flex-col items-center justify-center gap-10 py-20'>
         <div className='gradient-text text-5xl font-bold text-white'>Added features</div>
         <ConfettiGenerateLink />
         <ConfettiInstallment />
         <ConfettieSubscription />
-      </div> */}
+      </div>
       <CountryCoverageHome />
       <FAQSection />
       <CTASections />
